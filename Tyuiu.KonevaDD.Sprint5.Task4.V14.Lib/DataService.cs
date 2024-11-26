70d6ad177a15c3d02d2067f7757ac2733b0331de
Дарья Конева <Tyuiu.KonevaDD@outlook.com>
2024-11-26 15:34:06 +0500

    Task4

---
+++ Tyuiu.KonevaDD.Sprint5.Task4.V14.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+using System.Globalization;
+namespace Tyuiu.KonevaDD.Sprint5.Task4.V14.Lib
+{
+    public class DataService : ISprint5Task4V14
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string content;
+            using (StreamReader sr = new StreamReader(path))
+            {
+                content = sr.ReadToEnd().Trim();
+            }
+
+            double x;
+            if (!double.TryParse(content, NumberStyles.Any, CultureInfo.InvariantCulture, out x))
+            {
+                content = content.Replace(',', '.');
+            }
+
+            double y = Math.Sin(Math.Pow(x, 3)) + (2 / x);
+
+            return Math.Round(y, 3);
+        }
+    }
+}
---
+++ Tyuiu.KonevaDD.Sprint5.Task4.V14/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.KonevaDD.Sprint5.Task4.V14.Lib;
+using System.IO;
+namespace Tyuiu.KonevaDD.Sprint5.Task4.V14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнила: Конева Д. Д. | АСОиУб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Потоковый метод записи данных в бинарный файл                     *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #14                                                             *");
+            Console.WriteLine("* Выполнила: Конева Дарья Денисовна | АСОиУб-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан файл С:\\DataSprint5\\InPutDataFileTask4V0.txt                      *");
+            Console.WriteLine("* в котором есть вещественное значение.                                   *");
+            Console.WriteLine("* Прочитать значение из файла и подставить и вернуть полученный результат.*");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            string path;
+
+            Console.WriteLine("Введите путь к файлу:");
+
+            path = Console.ReadLine();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Создан! : " + ds.LoadFromDataFile(path));
+        }
+    }
+}
