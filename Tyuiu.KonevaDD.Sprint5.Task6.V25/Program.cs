70aaa988eba6955a921c7a8bfffe834030b0893a
Дарья Конева <Tyuiu.KonevaDD@outlook.com>
2024-11-27 21:54:25 +0500

    Task6

---
+++ Tyuiu.KonevaDD.Sprint5.Task6.V25/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.KonevaDD.Sprint5.Task6.V25.Lib;
+using System.IO;
+namespace Tyuiu.KonevaDD.Sprint5.Task6.V25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнила: Конева Д. Д. | АСОиУБ-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Обработка текстовых файлов                                        *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #25                                                             *");
+            Console.WriteLine("* Выполнила: Конева Дарья Денисовна | АСОиУБ-24-1                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан файл С:\\DataSprint5\\InPutDataFileTask6V25.txt в котором есть      *");
+            Console.WriteLine("* набор символьных данных. Найти количество знаков восклицания            *");
+            Console.WriteLine("* в заданной строке.                                                      *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            string path = Path.Combine("C:", "DataSprint5", "InPutDataFileTask6V25.txt");
+            Console.WriteLine("Данные находятся в файле: " + path);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine("Количество знаков восклицания в заданной строке = " + res);
+        }
+    }
+}
